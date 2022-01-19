4fe3d81f04d0b8d9e8b2643b5dde51039a2b1c65
Peiyu67 <1064C033@ntub.edu.tw>
2022-01-19 18:24:32 +0800

    MixManager

--- Assets/Animals/Item/ItemManager.cs
+++ Assets/Animals/Item/ItemManager.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager:MonoBehaviour
 {
+    public static ItemManager Instance = null;
     public List<Item> items;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i=0; i<items[2].gm.Length; i++)
+        if (Instance == null)
         {
-            Instantiate(items[2].gm[i],this.transform);
+            Instance = this;
         }
-        
     }
 
     // Update is called once per frame
--- Assets/Animals/Item/MixManager.cs
+++ Assets/Animals/Item/MixManager.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class MixManager : MonoBehaviour
 {
+    public List<Item> mixitems;
     // Start is called before the first frame update
     void Start()
-    {
+    {        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void CheckItemAndAdd(string tag)
+    {
+        foreach (var v in ItemManager.Instance.items)
+        {
+            if (tag == v.itemName)
+            {
+                mixitems.Add(v);
+            }
+        }
+    }
+    void CheckItemAndRemove(string tag)
+    {
+        foreach (var v in mixitems)
+        {
+            if (tag == v.itemName)
+            {
+                mixitems.Remove(v);
+            }
+        }
+    }
+
+    void MixItem()
+    {
+        int mix1 = mixitems[0].id;
+        int mix2 = mixitems[1].id;
+
+        if (mix1 == 0 && mix2 == 1)
+        {
+            mixitems.Clear();
+            Instantiate(ItemManager.Instance.items[2].gm[0] ,transform);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wood" || other.tag=="Leaf")
+        {
+            CheckItemAndAdd(other.tag);
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                MixItem();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wood" || other.tag == "Leaf")
+        {
+            CheckItemAndRemove(other.tag);
+        }
     }
 }
