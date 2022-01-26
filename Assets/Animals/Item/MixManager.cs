c5411d07aecb392b7b459bb6a18afe186653db94
Peiyu67 <1064C033@ntub.edu.tw>
2022-01-26 21:14:24 +0800

    SlotPosition

--- Assets/Animals/Item/MixManager.cs
+++ Assets/Animals/Item/MixManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MixManager : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class MixManager : MonoBehaviour
     public bool isMix;
     public string mix = "";
     public Item mixItem;
+    public bool isRight=true;
+    public GameObject[] slotimage;
+    public Sprite[] slotsprite = new Sprite[2];
     public Transform instantiate;
 
     void CheckItemAndAdd(string tag)
@@ -45,7 +49,7 @@ public class MixManager : MonoBehaviour
         mixItem = null;
     }
 
-    void  CanMixItem()
+    void CanMixItem()
     {
         mix = "";
         foreach (var v in mixitems)
@@ -74,19 +78,56 @@ public class MixManager : MonoBehaviour
     }
 
     void MixItem()
-    {      
+    {
         Instantiate(mixItem.gm[0], instantiate);
         ClearAll();
     }
 
+    void  SlotSelet(GameObject go)
+    {
+        Vector3 left = transform.TransformDirection(Vector3.up);
+        Vector3 toOther = go.transform.position - transform.position;
+        if (Vector3.Dot(left, toOther) > 0)
+        {
+            Debug.LogError("在右邊的位置");
+            slotimage[1].GetComponent<Image>().sprite = slotsprite[1];
+            slotimage[0].GetComponent<Image>().sprite = slotsprite[0];
+            isRight= true;
+        }
+        else
+        {
+            Debug.LogError("在左邊的位置");
+            slotimage[0].GetComponent<Image>().sprite = slotsprite[1];
+            slotimage[1].GetComponent<Image>().sprite = slotsprite[0];
+            isRight= false;
+        }
+    }
+    void SetObjectPosition(int i)
+    {
+        if (!isRight)
+        {
+            mixGameObject[i].transform.position = slotimage[0].transform.position;
+        }
+        else
+        {
+            mixGameObject[i].transform.position = slotimage[1].transform.position;
+        }
+    }    
+
     private void OnTriggerEnter(Collider other)
     {
+        if (other.tag == "Player")
+        {
+            SlotSelet(other.gameObject);
+        }
+
         foreach (var v in items)
         {
             if (v.canMix && other.tag == v.itemName)
             {
-                CheckItemAndAdd(other.tag);
+                CheckItemAndAdd(other.tag);             
                 mixGameObject.Add(other.gameObject);
+                SetObjectPosition(mixGameObject.Count-1);
             }
         }
         if (other.tag=="Player" && mixitems.Count>0)
@@ -96,6 +137,10 @@ public class MixManager : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
+        if (other.tag == "Player")
+        {
+            SlotSelet(other.gameObject);
+        }
         if (Input.GetKey(KeyCode.LeftControl) && isMix)
         {
             MixItem();
@@ -103,6 +148,11 @@ public class MixManager : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
+        if (other.tag == "Player")
+        {
+            slotimage[0].GetComponent<Image>().sprite = slotsprite[0];
+            slotimage[1].GetComponent<Image>().sprite = slotsprite[0];
+        }
         foreach (var v in items)
         {
             if (other.tag == v.itemName)
