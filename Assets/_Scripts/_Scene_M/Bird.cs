cc51d47b23f14e9587f10ad1db54ce29198c2634
Peiyu67 <1064C033@ntub.edu.tw>
2022-02-09 22:00:51 +0800

    UI

--- Assets/_Scripts/_Scene_M/Bird.cs
+++ Assets/_Scripts/_Scene_M/Bird.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Bird : MonoBehaviour
 {
-    Transform self;
-    [SerializeField] Vector3 offsetPosition;
+
 
     private void Start()
     {
-        self = GetComponent<Transform>();
+     
     }
 
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
-        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
-        RaycastHit hit;
-        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        SeagulPos();
+
+        //Vector3 mouse = Input.mousePosition;
+        //Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        //RaycastHit hit;
+        //if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        //{
+        //    if (hit.transform.tag == "Button")
+        //self.transform.position = hit.transform.position + offsetPosition;
+        //}
+    }
+    void SeagulPos()
+    {
+        string current = EventSystem.current.currentSelectedGameObject.name;
+        Debug.LogError(current);
+        switch (current)
         {
-            if (hit.transform.tag == "Button")
-                self.transform.position = hit.transform.position + offsetPosition;
+            case "NewGame":
+                Debug.LogError("EnterNewGame");
+                transform.position = new Vector3(889f, 261.7f, -1092f);
+                break;
+            case "Continue":
+                Debug.LogError("EnterContinue");
+                transform.position = new Vector3(889f, 259.6f, -1092f);
+                break;
+            case "Setting":
+                transform.position = new Vector3(889f, 258f, -1092f);
+                break;
+            case "Exit":
+                transform.position = new Vector3(889f, 256.8f, -1092f);
+                break;
         }
+
     }
 }
