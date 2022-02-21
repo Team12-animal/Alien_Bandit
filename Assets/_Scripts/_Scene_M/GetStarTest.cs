dba0cd69c7a8d7a027d4bbf01a351be2e7b8f1d7
Yeh TzuChen <67541201+uniyeh@users.noreply.github.com>
2022-02-21 21:48:42 +0800

    new scoreBoard

--- Assets/_Scripts/_Scene_M/GetStarTest.cs
+++ Assets/_Scripts/_Scene_M/GetStarTest.cs
@@ -7,10 +7,9 @@ public class GetStarTest : MonoBehaviour
     [SerializeField] LevelOneControl levelOneControl;
     [SerializeField] LevelTwoControl levelTwoControl;
     
-    public int collectRabbits; //catched rabbit amount
-    public int collectRaccoons; //catched raccoon amount
-    public int collectLittleRaccoons; //
-    public int collectPigs; //catched pig amount
+
+
+
 
     private void Awake()
     {
