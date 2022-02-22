1cd3712bafc5fcd72d833efa75658acc8dc9facf
Yeh TzuChen <67541201+uniyeh@users.noreply.github.com>
2022-02-23 04:55:53 +0800

    foxAI(bug left)

--- Assets/_Scripts/NPCAI/Fox_BehaviourTree.cs
+++ Assets/_Scripts/NPCAI/Fox_BehaviourTree.cs
@@ -24,30 +24,36 @@ public class Fox_BehaviourTree : MonoBehaviour
     private GameObject p2;
     private GameObject p3;
     private GameObject p4;
-    private AnimatorController ac;
+    private AnimatorController pAC;
 
     [SerializeField] public List<GameObject> players;
     [SerializeField]
-    public GameObject effectPlayer;
+    public List<GameObject> effectPlayers;
+    public GameObject nearestPlayer;
     public bool holdingRockOrBox;
 
     private float maxSpeed;
     private float maxRot;
     
     //seek ended or not
-    public bool arriveTarget = false;
+    private bool arriveTarget = false;
 
     //mission complete or not
-    public bool missionComplete = false;
+    private bool missionComplete = false;
 
     //player Action
     public bool pAttact = false;
+
+    //fox animator
+    private FoxAnimatorController fAC;
 
     // Start is called before the first frame update
     void Start()
     {
         DataInit();
         PlayerInit();
+
+        fAC = this.GetComponent<FoxAnimatorController>();
     }
 
     private void DataInit()
@@ -91,8 +97,13 @@ public class Fox_BehaviourTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("npc update");
+        if (!fAC.AllowToMove())
+        {
+            return;
+        }
+
         FindEffectPlayer();
+
         if (!missionComplete)
         {
             UpdateTargetPosition();
@@ -103,16 +114,15 @@ public class Fox_BehaviourTree : MonoBehaviour
         switch (status)
         {
             case (int)FoxAIData.FoxStatus.Safe:
-                Debug.Log("npc safe");
                 BreakItem();
                 break;
 
             case (int)FoxAIData.FoxStatus.Alert:
-                Alert(effectPlayer);
+                Alert(effectPlayers);
                 break;
 
             case (int)FoxAIData.FoxStatus.Attacked:
-
+                AvoidAttack();
                 break;
 
             case (int)FoxAIData.FoxStatus.Home:
@@ -140,7 +150,7 @@ public class Fox_BehaviourTree : MonoBehaviour
 
     private void CheckStatusAndUpdate()
     {
-        if(effectPlayer == null)
+        if(effectPlayers.Count == 0)
         {
             if(!missionComplete)
             {
@@ -153,9 +163,9 @@ public class Fox_BehaviourTree : MonoBehaviour
             }
         }
 
-        if(effectPlayer != null)
+        if(effectPlayers.Count > 0)
         {
-            if(ac.ThorowingOrNot() == false)
+            if(pAC.ThorowingOrNot() == false)
             {
                 status = (int)FoxAIData.FoxStatus.Alert;
             }
@@ -168,143 +178,267 @@ public class Fox_BehaviourTree : MonoBehaviour
         data.UpdateStatus(status);
     }
 
-    GameObject nearestPlayer;
-    float nearestDis = 10000.0f;
+    GameObject tempPlayer;
+    float nearestDist = 10000.0f;
     float tempDist;
     GameObject oriEffectPlayer;
     private void FindEffectPlayer()
     {
         //save original effectPlayer
-        if(effectPlayer != null)
+        if(nearestPlayer != null)
         {
-            oriEffectPlayer = effectPlayer;
+            oriEffectPlayer = nearestPlayer;
         }
 
         //reset compareData;
-        nearestDis = 10000.0f;
-        nearestPlayer = null;
+        nearestDist = 10000.0f;
+        tempPlayer = null;
+        effectPlayers.Clear();
 
         //find the nearest player
         foreach (GameObject p in players)
         {
             tempDist = (p.transform.position - this.transform.position).magnitude;
 
-            if (tempDist <= alertDist && tempDist <= nearestDis)
+            if (tempDist <= alertDist)
             {
-                nearestPlayer = p;
-                nearestDis = tempDist;
+                effectPlayers.Add(p);
+
+                if (tempDist <= nearestDist)
+                {
+                    tempPlayer = p;
+                    nearestDist = tempDist;
+                }
             }
         }
 
-        effectPlayer = nearestPlayer;
+        nearestPlayer = tempPlayer;
 
-        if(effectPlayer != null && effectPlayer != oriEffectPlayer)
+        if(nearestPlayer != null && nearestPlayer != oriEffectPlayer)
         {
-            ac = effectPlayer.GetComponent<AnimatorController>();
+            pAC = nearestPlayer.GetComponent<AnimatorController>();
         }
     }
 
     #region fox behaviour tree
     private void BreakItem()
     {
-        Debug.Log("npc break box");
         if(arriveTarget == false)
         {
-            Debug.Log("npc break box2");
             if (SteeringBehavior.CollisionAvoid(data) == false)
             {
-                Debug.Log("npc break box3");
                 arriveTarget = SteeringBehavior.Seek(data);
             }
 
             SteeringBehavior.Move(data);
+            fAC.ChangeAndPlayAnimation(fAC.runTrigger, data.m_fTempTurnForce, data.m_fMoveForce);
         }
         else
         {
-            Debug.Log("npc arrive");
-            BreakTarget(target);
-            missionComplete = true;
+            fAC.ChangeAndPlayAnimation(fAC.breaking, 0, 0);
         }
     }
 
-    private void Alert(GameObject player)
+    private void Alert(List<GameObject> players)
     {
-        AlertPlayer(effectPlayer);
+        AlertPlayer(players);
     }
 
-    private void AlertPlayer(GameObject player)
+    private void AlertPlayer(List<GameObject> players)
     {
-        if (effectPlayer == null)
+        if (players.Count < 1)
         {
             return;
         }
 
-        Vector3 pPos = effectPlayer.transform.position;
-        Vector3 pF = effectPlayer.transform.forward;
-        Vector3 foxPos = this.transform.position;
+        //if (players.Count == 1 && nearestPlayer != null) // fox walk around player
+        //{
+        //    Vector3 pPos = nearestPlayer.transform.position;
+        //    Vector3 pF = nearestPlayer.transform.forward;
+        //    Vector3 foxPos = this.transform.position;
 
-        Vector3 dir = foxPos - pPos;
-        float pFDotDir = Vector3.Dot(pF, dir);
+        //    Vector3 dir = foxPos - pPos;
+        //    float pFDotDir = Vector3.Dot(pF, dir);
 
-        if (pFDotDir > -0.3f) //fox in front of effectPlayer
+        //    if (pFDotDir > -0.3f) //fox in front of effectPlayer
+        //    {
+        //        RotateAround(nearestPlayer);
+        //    }
+        //    else
+        //    {
+        //        BreakItem();
+        //    }
+        //}
+
+        if (players.Count > 0)
         {
-            RotateAround(effectPlayer);
-        }
-        else
-        {
-            BreakItem();
+            AvoidPlayerAndRun(players);
+            fAC.ChangeAndPlayAnimation(fAC.trotTrigger, data.m_fTempTurnForce, data.m_fMoveForce);
         }
     }
 
     int left = 0;
     int right = 1;
+    bool avoided = false;
 
     private void AvoidAttack()
     {
-        if(effectPlayer == null)
+        if(nearestPlayer == null)
         {
             return;
         }
 
-        Vector3 pPos = effectPlayer.transform.position;
-        Vector3 pF = effectPlayer.transform.forward;
-        Vector3 foxPos = this.transform.position;
+        avoided = fAC.AvoidAttactEnd();
 
-        Vector3 dir = foxPos - pPos;
-        float pFDotDir = Vector3.Dot(pF, dir);
-
-        if(pFDotDir > 0.7f) //fox in effectPlayer attackable area
+        if (pAC.ThorowingOrNot() == true && avoided == false)
         {
-            int randomDir = Random.Range(0, 1);
-            Vector3 targetDir;
+            Vector3 pPos = nearestPlayer.transform.position;
+            Vector3 pF = nearestPlayer.transform.forward;
+            Vector3 foxPos = this.transform.position;
 
-            if(randomDir == right) //escape to right side
+            Vector3 dir = foxPos - pPos;
+            float pFDotDir = Vector3.Dot(pF, dir);
+
+            if (pFDotDir > 0.7f) //fox in effectPlayer attackable area
             {
-                targetDir = effectPlayer.transform.right;
-                targetDir.y = foxPos.y; //move on x-z
-            }
-            else
-            {
-                targetDir = -effectPlayer.transform.right;
-                targetDir.y = foxPos.y;
+                int randomDir = Random.Range(0, 1);
+                Vector3 targetDir;
+
+                if (randomDir == right) //escape to right side
+                {
+                    targetDir = nearestPlayer.transform.right;
+                    targetDir.y = foxPos.y; //move on x-z
+                }
+                else
+                {
+                    targetDir = -nearestPlayer.transform.right;
+                    targetDir.y = foxPos.y;
+                }
+
+                Vector3 newDir = targetDir + this.transform.forward;
+                this.transform.forward = Vector3.Slerp(this.transform.forward, newDir, 0.8f);
+                this.transform.position += this.transform.forward * data.jumpSpeed;
             }
 
-            Vector3 newDir = targetDir + this.transform.forward;
-            this.transform.forward = Vector3.Slerp(this.transform.forward, newDir, 0.8f);
-            this.transform.position += this.transform.forward * data.jumpSpeed;
+            fAC.ChangeAndPlayAnimation(fAC.jumpTrigger, 0, 0);
         }
         else
         {
-            this.transform.position += this.transform.forward * data.m_fMaxSpeed;
+            SteeringBehavior.Move(data);
+            fAC.ChangeAndPlayAnimation(fAC.runTrigger, data.m_fTempTurnForce, data.m_fMoveForce);
         }
     }
 
+    private void AvoidPlayerAndRun(List<GameObject> players)
+    {
+        if (SteeringBehavior.PlayerAvoid(data, players) == false && SteeringBehavior.CollisionAvoid(data) == false)
+        {
+            SteeringBehavior.Seek(data);
+        }
+
+        SteeringBehavior.Move(data);
+    }
+
+    private bool AvoidCollision(List<GameObject> players)
+    {
+        Debug.Log("npc avoid collision");
+        Vector3 foxPos = this.transform.position;
+        Vector3 foxF = this.transform.forward;
+        Vector3 vec;
+        float finalDotDist;
+        float finalProjDist;
+        Vector3 finalVec = Vector3.forward;
+        GameObject final = null;
+        float dist = 0.0f;
+        float dotf = 0.0f;
+        float finalDot = 0.0f;
+        int playerAmt = players.Count;
+
+        float minDist = 10000.0f;
+        for (int i = 0; i < playerAmt; i++)
+        {
+            vec = players[i].transform.position - foxPos;
+            vec.y = 0.0f;
+            dist = vec.magnitude;
+
+            if(dist > data.m_fProbeLength + 2.0f)
+            {
+                continue;
+            }
+
+            vec.Normalize();
+            dotf = Vector3.Dot(vec, foxF);
+
+            if (dotf < 0)
+            {
+                continue;
+            }
+            else if (finalDot > 1.0f)
+            {
+                dotf = 1.0f;
+            }
+
+            float projDist = dist * dotf;
+            float dotDist = Mathf.Sqrt(dist * dist - projDist * projDist);
+            if (dotDist > 2.0f + data.m_fRadius)
+            {
+                continue;
+            }
+
+            if (dist < minDist)
+            {
+                minDist = dist;
+                finalDotDist = dotDist;
+                finalProjDist = projDist;
+                finalVec = vec;
+                final = players[i];
+                finalDot = dotf;
+            }
+        }
+
+        if (final != null)
+        {
+            Vector3 cross = Vector3.Cross(foxF, finalVec);
+            float turnMag = Mathf.Sqrt(1.0f - finalDot * finalDot);
+
+            if (cross.y > 0.0f)
+            {
+                turnMag = -turnMag;
+            }
+
+            data.m_fTempTurnForce = turnMag;
+            float TotalLen = data.m_fProbeLength + 2.0f;
+            float ratio = minDist / TotalLen;
+
+            if(ratio > 1.0f)
+            {
+                ratio = 1.0f;
+            }
+
+            ratio = 1.0f - ratio;
+            data.m_fMoveForce = -ratio;
+            data.m_bCol = true;
+            data.m_bMove = false;
+            return true;
+        }
+
+        data.m_bCol = false;
+        return false;
+    }
+
+    private void MoveForward()
+    {
+        this.transform.position += this.transform.forward * data.m_fMaxSpeed;
+    }
+
+    bool arriveHome = false;
     private void GoHome()
     {
         target = data.birthPos;
         data.m_vTarget = target.transform.position;
 
-        bool arriveHome = SteeringBehavior.Seek(data);
+        MoveToBirthPos();
+        fAC.ChangeAndPlayAnimation(fAC.runTrigger, data.m_fTempTurnForce, data.m_fMoveForce);
 
         if (arriveHome)
         {
@@ -313,11 +447,21 @@ public class Fox_BehaviourTree : MonoBehaviour
         }
     }
 
+    private void MoveToBirthPos()
+    {
+        if (SteeringBehavior.CollisionAvoid(data) == false)
+        {
+            arriveHome = SteeringBehavior.Seek(data);
+        }
+
+        SteeringBehavior.Move(data);
+    }
+
     #endregion
 
     #region other behaviour
 
-    private void BreakTarget(GameObject target)
+    public void AnimaEventBreakTarget()
     {
         if (target.tag == "box")
         {
@@ -330,6 +474,9 @@ public class Fox_BehaviourTree : MonoBehaviour
         }
 
         target.SetActive(false);
+        missionComplete = true;
+
+        Debug.Log("npc break target" + missionComplete);
     }
 
     public float angularSpeed;
