140bc506e26c9ecf5c6ef7c520fec1d63ebaef00
KizunaMo <mo791206@gmail.com>
2022-02-26 18:24:42 +0800

    Wolf simple type

--- Assets/_Scripts/_Scene_M/WolfAI/SteeringBehavoirTest.cs
+++ Assets/_Scripts/_Scene_M/WolfAI/SteeringBehavoirTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SteeringBehavoir
+public class SteeringBehavoirTest
 {
     private const float nearDistance = 3.0f;
 
---
+++ Assets/_Scripts/_Scene_M/WolfAI/WolfController.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfController : MonoBehaviour
+{
+    [Header("GameObjects(Targets) in Scene")]
+    [SerializeField] GameObject restPlace;
+    [SerializeField] GameObject finalPlace;
+    GameObject[] rabbits;
+    bool catched;
+    bool rested;
+    bool finaled;
+    float restTime = 0.0f;
+
+    [Header("Self condition")]
+    [SerializeField] Transform mousePosition;
+
+    Animator animator;
+    Rigidbody rigidbody;
+
+    [Header("Using Others Scripts and Sources")]
+    public AIData_WOLF aiData;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+        catched = false;
+        rested = false;
+        finaled = false;
+    }
+
+    private void Update()
+    {
+        Vector3 temp = aiData.target.transform.position - transform.position;
+        if (!catched && animator.GetCurrentAnimatorStateInfo(0).IsName("Wolfr_Run_Forward") && (temp.magnitude) <= 3.6f)
+        {
+            animator.Play("Wolfr_Attack_JumpForward");
+        }
+        else if (!catched && animator.GetCurrentAnimatorStateInfo(0).IsName("Wolfr_Run_Forward") && (temp.magnitude) >= 3.0f)
+        {
+            SteeringBehavoirTest.Seek(aiData, aiData.target);
+            SteeringBehavoirTest.Move(aiData);
+        }
+        else if (catched && !rested)
+        {
+            aiData.target = restPlace;
+            SteeringBehavoirTest.Seek(aiData, aiData.target);
+            SteeringBehavoirTest.Move(aiData);
+            if (temp.magnitude <= 3.0f)
+            {
+                rested = true;
+                animator.Play("Wolfr_Sneak_Idle 0");
+                aiData.target = finalPlace;
+            }
+        }
+        else if(rested && restTime>= 6.0f && (temp.magnitude) >= 6.0f && !finaled)
+        {
+            animator.Play("Wolfr_Run_Forward 0 0");
+            SteeringBehavoirTest.Seek(aiData, aiData.target);
+            SteeringBehavoirTest.Move(aiData);
+            return;
+        }
+        else if (rested && restTime >= 6.0f && (temp.magnitude) <= 3.0f)
+        {
+            animator.applyRootMotion = false;
+            animator.Play("Wolfr_Jump_InPlace");
+            finaled = true;
+            return;
+        }
+
+        if (rested)
+        {
+            restTime += Time.deltaTime;
+        }
+
+        if(transform.position.y >= -288.74f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #region AnimationEvents
+    public void ApplyRootMotion()
+    {
+        animator.applyRootMotion = true;
+    }
+
+    public void CancelRooMotion()
+    {
+        animator.applyRootMotion = false;
+    }
+
+    public void AddForceToJump()
+    {
+        Vector3 go = transform.forward * 9.0f + transform.up * 12.0f;
+        rigidbody.AddForce(go, ForceMode.Impulse);
+    }
+
+    public void AddForceToFinalJump()
+    {
+        Vector3 go = transform.forward * 12.0f + transform.up * 54.0f;
+        rigidbody.AddForce(go, ForceMode.Impulse);
+    }
+
+    public void FindRabbits()
+    {
+        rabbits = GameObject.FindGameObjectsWithTag("Rabbit");
+        aiData.target = rabbits[0];
+    }
+
+    public void FindPlayers()
+    {
+
+    }
+
+    public void CatchRabbit()
+    {
+        rabbits[0].transform.SetParent(mousePosition, false);
+        rabbits[0].transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+        catched = true;
+    }
+
+
+    #endregion
+    //void OnAnimatorMove()
+    //{
+    //    Animator animator = GetComponent<Animator>();
+
+    //    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Wolfr_Sneak_Forward 0"))
+    //    {
+    //        animator.applyRootMotion = true;
+    //    }
+    //}
+}
